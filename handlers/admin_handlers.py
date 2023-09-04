f2ce9d57127d249a72f8c76779ca466a11780d32
zahar <pasechnik.pchelovod@mail.ru>
2023-09-04 10:13:45 +0300

    save db

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,16 +1,21 @@
 import asyncio
 
 import aiogram
+import redis
+import json
 from aiogram.filters import Text
 
 from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, FSInputFile
 from database.database import users_requests_db, users_db, save_users_db, save_users_requests_db, usernames_db
 from services.search_function import get_items, test_time
 from keyboards.delete_kb import create_delete_users_keyboard
+from config_data.config import admin_id
 
 from config_data.config import Config, load_config
 from aiogram import Bot
+
+r = redis.Redis(host='127.0.0.1', port=6379, db=1)
 
 
 API_URL: str = 'https://api.telegram.org/bot'
@@ -89,8 +94,6 @@ async def clear_users(user_id, name, sem: asyncio.Semaphore):
         except aiogram.exceptions.TelegramForbiddenError as e:
             print(e)
             users_to_delete[user_id] = name
-        # await asyncio.sleep(0.05)
-    # print(users_to_delete)
 
 
 @router.message(F.text == 'bot users clear')
@@ -119,39 +122,6 @@ async def delete(message: Message):
                                                   'cansel'))
 
 
-
-# @router.message(F.text == 'bot users clear')
-# async def clear_users(message: Message):
-#     for user_id, name in users_db.copy().items():
-#         if "<" in name or ">" in name:
-#             name = name.replace(">", "&gt;").replace("<", "&lt;")
-#         try:
-#             sent_message = await bot.send_message(chat_id=user_id, text="_", disable_notification=True)
-#             await bot.delete_message(chat_id=user_id, message_id=sent_message.message_id)
-#         except Exception as e:
-#             print(e)
-#             users_to_delete[user_id] = name
-#         # await asyncio.sleep(0.05)
-#     # print(users_to_delete)
-#
-#     message_dict = {}
-#
-#     for k, v in users_to_delete.items():
-#         if k in users_db:
-#             message_dict[v] = ""
-#
-#         if k in users_requests_db:
-#             message_dict[v] = users_requests_db[k]['request']
-#
-#     answer = [f"{k}: {v}\n" for k, v in message_dict.items()]
-#
-#     stat = ''.join(answer)
-#     await message.answer(
-#         f"{stat}users to delete: {len(users_to_delete)}",
-#         reply_markup=create_delete_users_keyboard('delete',
-#                                                   'cansel'))
-
-
 @router.callback_query(Text(text='delete'))
 async def delete(callback: CallbackQuery):
     id_to_delete = set(users_db.keys()) & set(users_to_delete.keys())
@@ -175,3 +145,25 @@ async def cansel(callback: CallbackQuery):
 @router.message(F.text == 'bot test time')
 async def test_time_users(message: Message):
     await test_time()
+
+
+@router.message(F.text == 'bot save db')
+async def save_db(message: Message):
+
+    if message.from_user.id == admin_id:
+
+        with open('user_dict.json', 'w', encoding='utf-8-sig') as fl:
+            json.dump(users_db, fl, indent=4, ensure_ascii=False)
+
+        with open('usernames_dict.json', 'w', encoding='utf-8-sig') as fl:
+            json.dump(usernames_db, fl, indent=4, ensure_ascii=False)
+
+        with open('user_requests.json', 'w', encoding='utf-8-sig') as fl:
+            json.dump(users_requests_db, fl, indent=4, ensure_ascii=False)
+
+        file = FSInputFile('user_dict.json')
+        file_1 = FSInputFile('usernames_dict.json')
+        file_2 = FSInputFile('user_requests.json')
+        await message.answer_document(file)
+        await message.answer_document(file_1)
+        await message.answer_document(file_2)
