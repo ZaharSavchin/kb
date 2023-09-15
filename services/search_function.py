625c605045e7626c82f820b166967699dd340643
zahar <pasechnik.pchelovod@mail.ru>
2023-09-15 14:03:37 +0300

    test

--- services/search_function.py
+++ services/search_function.py
@@ -18,73 +18,69 @@ bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
 
 
 async def get_items():
-    await bot.send_message(chat_id=admin_id, text='цикл запущен')
+    await bot.send_message(chat_id=admin_id, text='цикл запущен, start')
     while True:
         for user_id, request in users_requests_db.copy().items():
-            try:
-                ua = UserAgent()
-                fake_ua = {'user-agent': ua.random}
-                # Запрашиваем HTML-код страницы
-                if request['request'].startswith('https:'):
-                    result = requests.get(f"{request['request']}", headers=fake_ua, timeout=3)
-                    soup = BeautifulSoup(result.text, "html.parser")
-                    # Получаем ссылки на последние 5 товаров
-                    sections = soup.find_all("section")[:2]
-                    for section in sections:
-                        section = section.find_all("a", class_=re.compile(r"styles_wrapper__[a-zA-Z0-9]+$"))[:2]
-                        for element in section:
-                            title = element.text
-                            if "<" in title or ">" in title:
-                                title = title.replace(">", "&gt;").replace("<", "&lt;")
-                            link = element.get("href").split('?')[0]
-                            item = f"{title} {link}"
-                            if item[:20] not in request['user_items']:
-                                request['user_items'].append(item[:20])
+            # Запрашиваем HTML-код страницы
+            if request['request'].startswith('https:'):
+                result = requests.get(f"{request['request']}")
+                soup = BeautifulSoup(result.text, "html.parser")
+                # Получаем ссылки на последние 5 товаров
+                sections = soup.find_all("section")[:2]
+                for section in sections:
+                    section = section.find_all("a", class_=re.compile(r"styles_wrapper__[a-zA-Z0-9]+$"))[:2]
+                    for element in section:
+                        title = element.text
+                        if "<" in title or ">" in title:
+                            title = title.replace(">", "&gt;").replace("<", "&lt;")
+                        link = element.get("href")
+                        item = f"{title} {link}"
+                        if item[:20] not in request['user_items']:
+                            request['user_items'].append(item[:20])
+                            try:
+                                image_url = re.search('data-src="(.*?)"', str(element)).group(1)
+                                await bot.send_photo(chat_id=user_id, photo=image_url, caption=item)
+                                # requests.get(
+                                #     f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={user_id}&photo={image_url}&caption={item}')
+                            except Exception as e:
+                                print(f'e={e}')
                                 try:
-                                    image_url = re.search('data-src="(.*?)"', str(element)).group(1)
-                                    await bot.send_photo(chat_id=user_id, photo=image_url, caption=item)
-                                    # requests.get(
-                                    #     f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={user_id}&photo={image_url}&caption={item}')
-                                except Exception as e:
-                                    print(f'e={e}')
-                                    try:
-                                        await bot.send_message(chat_id=user_id, text=item)
-                                    except Exception as err:
-                                        print(err)
-                                    # requests.get(
-                                    #     f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={user_id}&text={item}')
-                    await asyncio.sleep(0.1)
-                else:
-                    result = requests.get(f"https://www.kufar.by/l{request['region']}?cmp=0&ot=1&query={request['request']}&sort=lst.d", headers=fake_ua, timeout=3)
-                    # Разбираем HTML-код с помощью BeautifulSoup
-                    soup = BeautifulSoup(result.text, "html.parser")
-                    # Получаем ссылки на последние 5 товаров
-                    sections = soup.find_all("section")[:4]
-                    for section in sections:
-                        section = section.find_all("a", class_=re.compile(r"styles_wrapper__[a-zA-Z0-9]+$"))
-                        for element in section:
-                            title = element.text
-                            if "<" in title or ">" in title:
-                                title = title.replace(">", "&gt;").replace("<", "&lt;")
-                            link = element.get("href").split('?')[0]
-                            item = f"{title} {link}"
-                            if item[:20] not in request['user_items']:
-                                request['user_items'].append(item[:20])
+                                    await bot.send_message(chat_id=user_id, text=item)
+                                except Exception as err:
+                                    print(err)
+                                # requests.get(
+                                #     f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={user_id}&text={item}')
+                await asyncio.sleep(0.1)
+            else:
+                result = requests.get(
+                    f"https://www.kufar.by/l{request['region']}?cmp=0&ot=1&query={request['request']}&sort=lst.d")
+                # Разбираем HTML-код с помощью BeautifulSoup
+                soup = BeautifulSoup(result.text, "html.parser")
+                # Получаем ссылки на последние 5 товаров
+                sections = soup.find_all("section")[:4]
+                for section in sections:
+                    section = section.find_all("a", class_=re.compile(r"styles_wrapper__[a-zA-Z0-9]+$"))
+                    for element in section:
+                        title = element.text
+                        if "<" in title or ">" in title:
+                            title = title.replace(">", "&gt;").replace("<", "&lt;")
+                        link = element.get("href")
+                        item = f"{title} {link}"
+                        if item[:20] not in request['user_items']:
+                            request['user_items'].append(item[:20])
+                            try:
+                                image_url = re.search('data-src="(.*?)"', str(element)).group(1)
+                                await bot.send_photo(chat_id=user_id, photo=image_url, caption=item)
+                                # requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={user_id}&photo={image_url}&caption={item}')
+                            except Exception as e:
+                                print(f'e={e}')
                                 try:
-                                    image_url = re.search('data-src="(.*?)"', str(element)).group(1)
-                                    await bot.send_photo(chat_id=user_id, photo=image_url, caption=item)
-                                    # requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={user_id}&photo={image_url}&caption={item}')
-                                except Exception as e:
-                                    print(f'e={e}')
-                                    try:
-                                        await bot.send_message(chat_id=user_id, text=item)
-                                    except Exception as err:
-                                        print(err)
-                                    # requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={user_id}&text={item}')
-                    await asyncio.sleep(0.1)
-            except Exception as err:
-                print(err)
-            await asyncio.sleep(5)
+                                    await bot.send_message(chat_id=user_id, text=item)
+                                except Exception as err:
+                                    print(err)
+                                # requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={user_id}&text={item}')
+                await asyncio.sleep(0.1)
+            counter += 1
 
 
 async def test_time():
