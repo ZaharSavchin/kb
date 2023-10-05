0b8480b6cf4447f3b7c5cfcec73d3294be58d49c
ZaharSavchin <115952519+ZaharSavchin@users.noreply.github.com>
2023-10-05 10:09:03 +0300

    try

--- services/new_search_function.py
+++ services/new_search_function.py
@@ -10,37 +10,40 @@ from config_data.config import admin_id
 
 async def new_search(session: aiohttp.ClientSession, sem, user_id, request):
     async with sem:
-        url = f"https://www.kufar.by/l{request['region']}?cmp=0&ot=1&query={request['request']}&sort=lst.d"
-        if request['request'].startswith('https:') and 'kufar' in request['request']:
-            url = request['request']
-        async with session.get(url) as response:
-            resp = await response.text()
-            soup = BeautifulSoup(resp, 'html.parser')
-            sections = soup.find_all("section")[:4]
-            for section in sections:
-                section = section.find_all("a", class_=re.compile(r"styles_wrapper__[a-zA-Z0-9]+$"))
-                for element in section:
-                    title = element.text
-                    if len(title) > 300:
-                        title = f'{title[:300]}...'
-                    if "<" in title or ">" in title:
-                        title = title.replace(">", "&gt;").replace("<", "&lt;")
-                    link = element.get("href").split('?')[0]
-                    item = f"{title}\n{link}"
-                    check = item
-                    if '$' in title:
-                        check = link
-                    if check not in request['user_items']:
-                        request['user_items'].append(check)
-                        try:
-                            image_url = re.search('data-src="(.*?)"', str(element)).group(1)
-                            await bot.send_photo(chat_id=user_id, photo=image_url, caption=item)
-                        except Exception as e:
-                            print(f'e={e}')
+        try:
+            url = f"https://www.kufar.by/l{request['region']}?cmp=0&ot=1&query={request['request']}&sort=lst.d"
+            if request['request'].startswith('https:') and 'kufar' in request['request']:
+                url = request['request']
+            async with session.get(url) as response:
+                resp = await response.text()
+                soup = BeautifulSoup(resp, 'html.parser')
+                sections = soup.find_all("section")[:4]
+                for section in sections:
+                    section = section.find_all("a", class_=re.compile(r"styles_wrapper__[a-zA-Z0-9]+$"))
+                    for element in section:
+                        title = element.text
+                        if len(title) > 300:
+                            title = f'{title[:300]}...'
+                        if "<" in title or ">" in title:
+                            title = title.replace(">", "&gt;").replace("<", "&lt;")
+                        link = element.get("href").split('?')[0]
+                        item = f"{title}\n{link}"
+                        check = item
+                        if '$' in title:
+                            check = link
+                        if check not in request['user_items']:
+                            request['user_items'].append(check)
                             try:
-                                await bot.send_message(chat_id=user_id, text=item)
-                            except Exception as err:
-                                print(err)
+                                image_url = re.search('data-src="(.*?)"', str(element)).group(1)
+                                await bot.send_photo(chat_id=user_id, photo=image_url, caption=item)
+                            except Exception as e:
+                                print(f'e={e}')
+                                try:
+                                    await bot.send_message(chat_id=user_id, text=item)
+                                except Exception as err:
+                                    print(err)
+        except Exception as err:
+            print(f'GET or SOUP err: {err}, ({url})')
 
 
 async def new_search_monitor(num_sem):
