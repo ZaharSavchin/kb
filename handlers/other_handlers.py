a5d67134e96893b3c22f19c5041f62e8cc7e7a45
zahar <pasechnik.pchelovod@mail.ru>
2023-11-04 21:32:59 +0300

    send id to admin when botton prest

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -3,6 +3,9 @@ from aiogram.filters import Text
 from aiogram.types import Message, CallbackQuery
 from lexicon.lexicon import LEXICON
 from config_data.logging_utils import logger
+from config_data.config import admin_id
+from handlers.admin_handlers import bot
+from database.database import users_db, usernames_db
 
 router = Router()
 
@@ -16,7 +19,9 @@ async def by_slots(callback: CallbackQuery):
                                        f' - <b>Второй</b> слот стоит 5 BYN в месяц\n'
                                        f' - <b>Каждый</b> последующий 2.5 BYN в месяц\n'
                                        f' - При покупке любого количества слотов частота отслеживания всех запросов и ссылок (включая первый бесплатный) около <b>1 минуты</b>\n\n'
-                                       f' - Для получения реквизитов для оплаты напишите сюда @help_enot нужное Вам количество слотов и это число (<b>{user_id}</b> - это Ваш id в боте)')
+                                       f' - Для получения реквизитов для оплаты напишите сюда @help_enot нужное Вам количество слотов')
+    mes_too_admin = f'{users_db[user_id]}, @{usernames_db[user_id]}, {user_id}'.replace(">", "&gt;").replace("<", "&lt;")
+    await bot.send_message(chat_id=admin_id, text=mes_too_admin)
     await callback.answer()
 
 
